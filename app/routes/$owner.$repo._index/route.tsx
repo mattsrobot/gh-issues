973b1b65b197c6406e990c8debf67c1e0e8b395d
Matt <mattsrobot@users.noreply.github.com>
2024-02-22 23:50:53 +0000

    remove dead code

--- app/routes/$owner.$repo._index/route.tsx
+++ app/routes/$owner.$repo._index/route.tsx
@@ -1,14 +1,14 @@
 import type { MetaFunction } from "@remix-run/node";
-import { json, LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
-import { Card, List, ListHeader, Button, Flex, Text } from "~/components";
-import { useLoaderData, useSearchParams, useFetcher } from "@remix-run/react";
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { List, ListHeader, Button, Flex, Text } from "~/components";
+import { useLoaderData, useSearchParams } from "@remix-run/react";
 import { RequestError, Octokit } from "octokit";
 import { IssueOpenedIcon } from '@primer/octicons-react';
 import IssueCard from "./issue-card";
 import logger from "~/components/logger";
 import { createTokenAuth } from "@octokit/auth-token";
 import { CenteredContent } from "~/layouts";
-import { fetchIssuesQuery, IssuesResponse, Issue } from "~/api/fetchIssues";
+import { fetchIssuesQuery, IssuesResponse } from "~/api/fetchIssues";
 import { searchIssues, SearchResponse } from "~/api/searchIssues";
 import { useCallback, useEffect, useState } from "react";
 import { TopNavigation, SearchTextField } from "~/navigation";
@@ -26,69 +26,6 @@ export const meta: MetaFunction = ({ params }) => {
     ];
 };
 
-export async function action({ request, params }: ActionFunctionArgs) {
-    const repo = params.repo ?? "";
-    const owner = params.owner ?? "";
-
-    const logRequest = logger.child({ repo, owner, });
-    logRequest.info('🏃 starting search issues');
-
-    try {
-        const auth = createTokenAuth(process.env.GITHUB_AUTH_TOKEN!);
-
-        const { token } = await auth();
-
-        const octokit = new Octokit({
-            auth: token,
-        });
-
-        const body = await request.formData();
-
-        const url = new URL(request.url);
-        const state = url.searchParams.get("state") ?? "open";
-
-        const searchQuery = `repo:${owner}/${repo} type:issue state:${state} in:title sort:createdAt-desc ${body.get("query")}`;
-
-        logRequest.info(`🏃 searching ${searchQuery}`);
-
-        const data: SearchResponse = await octokit.graphql(searchIssues, {
-            owner,
-            name: repo,
-            searchQuery,
-        });
-
-        logRequest.info('✅ completed searching issues');
-
-        console.log(data);
-
-        return json({
-            owner,
-            repo,
-            data,
-            error: null
-        });
-
-    } catch (error) {
-
-        console.error(error);
-
-        const logError = logger.child({
-            repo,
-            owner,
-            error
-        });
-
-        logError.error('🔥 had an error searching issues');
-
-        return json({
-            owner,
-            repo,
-            data: null,
-            error: error
-        });
-    }
-}
-
 export async function loader({ request, params }: LoaderFunctionArgs) {
     const repo = params.repo ?? "";
     const owner = params.owner ?? "";
@@ -104,26 +41,53 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 
         const url = new URL(request.url);
         const state = url.searchParams.get("state") ?? "open";
+        const query = url.searchParams.get("q") ?? "";
 
         const logRequest = logger.child({ repo, owner, });
         logRequest.info('🏃 starting fetching issues');
 
-        const response: IssuesResponse = await octokit.graphql(fetchIssuesQuery, {
-            owner,
-            name: repo,
-            states: [state.toUpperCase()]
-        });
 
-        const data = response.repository
+        if (query.length > 0) {
 
-        logRequest.info('✅ completed fetching issues');
+            const searchQuery = `repo:${owner}/${repo} type:issue state:${state} in:title sort:createdAt-desc ${query}`;
 
-        return json({
-            owner,
-            repo,
-            data,
-            error: null
-        });
+            logRequest.info(`🏃 searching ${searchQuery}`);
+
+            const searchData: SearchResponse = await octokit.graphql(searchIssues, {
+                owner,
+                name: repo,
+                searchQuery,
+            });
+
+            logRequest.info('✅ completed searching issues');
+
+            return json({
+                owner,
+                repo,
+                data: null,
+                searchData,
+                error: null
+            });
+
+        } else {
+            const response: IssuesResponse = await octokit.graphql(fetchIssuesQuery, {
+                owner,
+                name: repo,
+                states: [state.toUpperCase()]
+            });
+
+            const data = response.repository
+
+            logRequest.info('✅ completed fetching issues');
+
+            return json({
+                owner,
+                repo,
+                data,
+                searchData: null,
+                error: null
+            });
+        }
 
     } catch (error) {
         let status, message;
@@ -150,6 +114,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
             owner,
             repo,
             data: null,
+            searchData: null,
             error: message
         });
     }
@@ -157,22 +122,18 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 
 export default function Index() {
 
-    const { data, error, repo, owner } = useLoaderData<typeof loader>();
+    const { data, searchData, error, repo, owner } = useLoaderData<typeof loader>();
 
     const [issues, setIssues] = useState(data?.issues?.nodes ?? []);
 
-    const searchFetcher = useFetcher<typeof action>({ key: "search" });
-
-    const actionData = searchFetcher.data;
-
-    const openCount = data?.openIssues?.totalCount ?? "";
-    const closedCount = data?.closedIssues?.totalCount ?? "";
+    const openCount = "";
+    const closedCount = "";
 
     const [searchParams, setSearchParams] = useSearchParams();
 
     const state = searchParams.get("state") ?? "open";
 
-    const updateState = useCallback((s: IssueState) => {
+    const updateIssueState = useCallback((s: IssueState) => {
         searchParams.set("state", s);
         setSearchParams(searchParams, {
             preventScrollReset: true,
@@ -181,31 +142,28 @@ export default function Index() {
 
     const [searchInput, setSearchInput] = useState('')
 
-    const debouncedSearchInput = useDebounceCallback(setSearchInput, 500);
+    const debouncedSearchInput = useDebounceCallback(setSearchInput, 200);
 
     useEffect(() => {
-        if (searchInput.length == 0) {
-            setIssues(data?.issues?.nodes ?? []);
-            return;
-        }
+        searchParams.set("q", searchInput);
 
-        const postData = {
-            __action: "search",
-            query: searchInput
-        }
-
-        searchFetcher.submit(postData, {
-            method: "post",
+        setSearchParams(searchParams, {
+            preventScrollReset: true,
         });
+
     }, [searchInput]);
 
+    useEffect(() => {
+        if (!!data) {
+            setIssues(data.issues?.nodes ?? []);
+        }
+    }, [data]);
 
     useEffect(() => {
-        if (actionData) {
-            const changes = actionData.data?.search?.nodes ?? [];
-            setIssues(changes);
+        if (!!searchData) {
+            setIssues(searchData.search?.nodes ?? []);
         }
-    }, [actionData]);
+    }, [searchData]);
 
     return (
         <>
@@ -224,12 +182,12 @@ export default function Index() {
             <CenteredContent>
                 <List>
                     <ListHeader>
-                        <Button variant="ghost" muted={state == "closed"} onClick={() => updateState("open")}>
+                        <Button variant="ghost" muted={state == "closed"} onClick={() => updateIssueState("open")}>
                             <Flex direction="row" align="center" gap="2">
                                 <IssueOpenedIcon size={15} /> Open {openCount}
                             </Flex>
                         </Button>
-                        <Button variant="ghost" muted={state == "open"} onClick={() => updateState("closed")}>
+                        <Button variant="ghost" muted={state == "open"} onClick={() => updateIssueState("closed")}>
                             Closed {closedCount}
                         </Button>
                     </ListHeader>
